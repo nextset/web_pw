22923f7a07119f971133c965019e50d650c9ccf6
nextset <71253202+nextset@users.noreply.github.com>
2024-05-11 22:22:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def pw():
     import random
     pas1 = list('ABCDEFGHIGKLMNopqrstuvyxwz123456')
-    pas1 = list('abcdefghigklmnOPQRSTUVYXWZ7890')
+    pas2 = list('abcdefghigklmnOPQRSTUVYXWZ7890')
     random.shuffle(pas2)
     random.shuffle(pas3)
     passwd = 'WN_' + ''.join([random.choice(pas1) for x in range(11)] + [random.choice(pas2) for x in range(11)])
