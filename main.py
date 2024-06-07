1e3d447ae099ee5b12cd2a153cf069161bbb39c0
Igor <nextset2005@yandex.ru>
2024-06-07 11:22:28 +0300

    main fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def pw():
     import random
     pas1 = list('ABCDEFGHIGKLMNopqrstuvyxwz123456')
-    pas2 = list('abcdefghigklmnOPQRSTUVYXWZ7890')
+    pas2 = list('abcdefghigklmnOPQRSTUVYXWZ7890@$')
     random.shuffle(pas1)
     random.shuffle(pas2)
     passwd = 'WN_' + ''.join([random.choice(pas1) for x in range(11)] + [random.choice(pas2) for x in range(11)])
